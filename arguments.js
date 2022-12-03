21d5ca0cfe3bbfb36310299d71a77394ab25a176
chota-bheem-codes <mrwt005@gmail.com>
2022-12-03 18:10:22 +0530

    hardhat template

---
+++ arguments.js
@@ -0,0 +1,38 @@
+/*
+module.exports = 
+[
+  "0x8c929EA5BECAa4f2007B26578Fa33a3DC421F167",
+  "0x8c929EA5BECAa4f2007B26578Fa33a3DC421F167",
+  "0x2eEe3947c2747Aa6206B64064eb3AB1ddf0BC35b",
+]
+for game factory contract
+ */
+
+// const encodedID = id => ethers.utils.keccak256(ethers.utils.toUtf8Bytes(id));
+
+// const epochConverter = date => Math.round(new Date(date).getTime() / 1000.0);
+
+// const changeDateFormat = date => {
+//   const dateTime = date.split(" ");
+//   const dateSplit = dateTime[0].split("-");
+//   const dateMmDdYyyy = dateSplit[1] + "-" + dateSplit[0] + "-" + dateSplit[2] + " " + dateTime[1];
+//   return dateMmDdYyyy;
+// };
+
+// const getEpoch = date => epochConverter(changeDateFormat(date));
+
+// module.exports = [
+//   encodedID("G1"),
+//   "0x8c929EA5BECAa4f2007B26578Fa33a3DC421F167",
+//   "0x8c929EA5BECAa4f2007B26578Fa33a3DC421F167",
+//   "0x2eEe3947c2747Aa6206B64064eb3AB1ddf0BC35b",
+//   3,
+//   getEpoch("20-09-2021 03:00:00"),
+//   getEpoch("30-09-2021 03:00:00"),
+// ];
+
+module.exports = [
+  "0x8c929EA5BECAa4f2007B26578Fa33a3DC421F167",
+  "0x8c929EA5BECAa4f2007B26578Fa33a3DC421F167",
+  "0x2eEe3947c2747Aa6206B64064eb3AB1ddf0BC35b",
+];
---
+++ hardhat.config.ts
@@ -0,0 +1,140 @@
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+import "./tasks/accounts";
+import "./tasks/clean";
+import "./tasks/deployers";
+import "@openzeppelin/hardhat-upgrades";
+
+import { resolve } from "path";
+
+import { config as dotenvConfig } from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import { NetworkUserConfig } from "hardhat/types";
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+const chainIds = {
+  ganache: 1337,
+  goerli: 5,
+  hardhat: 31337,
+  kovan: 42,
+  mainnet: 1,
+  rinkeby: 4,
+  ropsten: 3,
+  polygon: 137,
+  mumbai: 80001,
+  cronos: 338,
+  shardeum: 8080
+};
+
+// Ensure that we have all the environment variables we need.
+const mnemonic: string | undefined = process.env.MNEMONIC;
+if (!mnemonic) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
+
+const infuraApiKey: string | undefined = process.env.INFURA_API_KEY;
+if (!infuraApiKey) {
+  throw new Error("Please set your INFURA_API_KEY in a .env file");
+}
+
+const etherscanApiKey: string | undefined = process.env.ETHERSCAN_KEY;
+if (!etherscanApiKey) {
+  throw new Error("Please set your ETHERSCAN_KEY in a .env file");
+}
+
+function getChainConfig(network: keyof typeof chainIds): NetworkUserConfig {
+  let url = "";
+  url = "https://" + network + ".infura.io/v3/" + infuraApiKey;
+  if (network == "polygon") {
+    url = "https://polygon-rpc.com";
+  }
+  if (network == "mumbai") {
+    url = "https://matic-mumbai.chainstacklabs.com";
+  }
+  if (network == "cronos") {
+    url = "https://evm-t3.cronos.org";
+  }
+  if (network == "shardeum") {
+    url = "https://explorer-liberty10.shardeum.org";
+  }
+  return {
+    accounts: {
+      count: 10,
+      mnemonic,
+      path: "m/44'/60'/0'/0",
+      initialIndex: 0,
+    },
+    chainId: chainIds[network],
+    url,
+  };
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    excludeContracts: [],
+    src: "./contracts",
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic,
+      },
+      chainId: chainIds.hardhat,
+      // See https://github.com/sc-forks/solidity-coverage/issues/652
+      hardfork: process.env.CODE_COVERAGE ? "berlin" : "london",
+    },
+    goerli: getChainConfig("goerli"),
+    kovan: getChainConfig("kovan"),
+    rinkeby: getChainConfig("rinkeby"),
+    ropsten: getChainConfig("ropsten"),
+    ganache: {
+      url: "http://127.0.0.1:8545",
+      // accounts: [privateKey1, privateKey2, ...]
+    },
+    polygon: getChainConfig("polygon"),
+    mumbai: getChainConfig("mumbai"),
+    cronos: getChainConfig("cronos"),
+    shardeum: getChainConfig("shardeum")
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  etherscan: {
+    apiKey: etherscanApiKey,
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/solidity-template/issues/31
+        bytecodeHash: "none",
+      },
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 1000000,
+      },
+    },
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+};
+
+export default config;
